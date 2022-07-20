ea60deb6cdb0c72912cf9a4ca3596c443fa613c8
foranewworld <58407115+foranewworld@users.noreply.github.com>
2022-07-20 16:04:26 +0100

    Add files via upload

--- package main.go
+++ package main.go
@@ -2,88 +2,52 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/junctional/GoJo/gojo/junction"
 	"github.com/junctional/GoJo/gojo/types"
 )
 
-func getMerge[T any]() (func(T), func(T), func(types.Unit) (T, error)) {
+func getMutex() (func(types.Unit), func(types.Unit) (types.Unit, error)) {
 	j := junction.NewJunction()
 
-	inputPort1, produce1 := junction.NewAsyncPort[T](j)
-	inputPort2, produce2 := junction.NewAsyncPort[T](j)
-	outputPort, consume := junction.NewSyncPort[types.Unit, T](j)
+	releasePort, release := junction.NewAsyncPort[types.Unit](j)
+	acquirePort, acquire := junction.NewSyncPort[types.Unit, types.Unit](j)
+	lockPort, lock := junction.NewAsyncPort[types.Unit](j)
 
-	junction.NewBinarySyncJoinPattern[T, types.Unit, T](inputPort1, outputPort).Action(func(value T, b types.Unit) T {
-		return value
+	junction.NewBinarySyncJoinPattern[types.Unit, types.Unit, types.Unit](lockPort, acquirePort).Action(func(a types.Unit, b types.Unit) types.Unit {
+		return types.Unit{}
 	})
-	junction.NewBinarySyncJoinPattern[T, types.Unit, T](inputPort2, outputPort).Action(func(value T, b types.Unit) T {
-		return value
-	})
-	return produce1, produce2, consume
-}
-func getZip[S any, T any]() (func(S), func(T), func(types.Unit) (struct {
-	first  S
-	second T
-}, error)) {
-	j := junction.NewJunction()
 
-	inputPort1, produce1 := junction.NewAsyncPort[S](j)
-	inputPort2, produce2 := junction.NewAsyncPort[T](j)
-	outputPort, consume := junction.NewSyncPort[types.Unit, struct {
-		first  S
-		second T
-	}](j)
-
-	junction.NewTernarySyncJoinPattern[S, T, types.Unit, struct {
-		first  S
-		second T
-	}](inputPort1, inputPort2, outputPort).Action(func(value1 S, value2 T, _ types.Unit) struct {
-		first  S
-		second T
-	} {
-		return struct {
-			first  S
-			second T
-		}{first: value1, second: value2}
+	junction.NewUnaryAsyncJoinPattern[types.Unit](releasePort).Action(func(a types.Unit) {
+		lock(types.Unit{})
 	})
-	return produce1, produce2, consume
+
+	lock(types.Unit{})
+
+	return release, acquire
 }
 
 func main() {
-	produce1, produce2, consume := getZip[string, int]()
-	// Writer1
-	go func() {
-		val := "A"
-		for i := 0; i < 5; i++ {
-			time.Sleep(100)
-			fmt.Println("Producing: ", val)
-			produce1(val)
-			//val += 1
-		}
-	}()
+	release, acquire := getMutex()
 
-	// Writer2
-	go func() {
-		val := 0
-		for i := 0; i < 5; i++ {
-			time.Sleep(100)
-			fmt.Println("Producing: ", val)
-			produce2(val)
-			val += 1
-		}
-	}()
+	sharedVar := 0
 
-	// Reader
-	for i := 0; i < 3; i++ {
-		go func(num int) {
-			for true {
-				val, _ := consume(types.Unit{})
+	for i := 0; i < 10; i += 2 {
+		go func() {
+			acquire(types.Unit{})
+			sharedVar += 2
+			fmt.Println("Incrementing: ", sharedVar)
+			release(types.Unit{})
+		}()
+	}
 
-				fmt.Println(num, " consuming : ", val)
-			}
-		}(i)
+	for i := 1; i < 10; i += 2 {
+		go func() {
+			acquire(types.Unit{})
+			sharedVar -= 2
+			fmt.Println("Decrementing: ", sharedVar)
+			release(types.Unit{})
+		}()
 	}
 
 	for true {
